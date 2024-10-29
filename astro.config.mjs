98ecb312cbbee6f9956ac32e795a9055d33fe041
Charles Munat <charles.munat@gmail.com>
2024-10-29 15:36:07 +1300

    chore: initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import basicSsl from "@vitejs/plugin-basic-ssl"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	devToolbar: {
+		enabled: false,
+	},
+	markdown: {
+		syntaxHighlight: "prism",
+	},
+	vite: {
+		plugins: [
+			basicSsl({
+				name: "localhost",
+				domains: ["*.localhost", "localhost"],
+				certDir: "/Users/guy/.ssh",
+			}),
+		],
+		server: {
+			https: true,
+		},
+	}
+});
